ae60ccefcc0f5570909ef72d060919166c0bbf0d
Hrvoje Kopinč <hrvoje.kopinc@gmail.com>
2015-02-07 20:12:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,12 @@ dir=$(cd "$(dirname "$0")"; pwd)
 
 for file in $files; do
     #backup files
-    echo "mv ~/.$file $dir/$file.bak"
-    mv ~/.$file $dir/$file.bak
+    if [ -f ~/.$file ]; then
+        echo "Moving .$file from ~/ to $dir"
+        mv ~/.$file $dir/$file.bak
+    fi
 
     #create links
-    echo "ln -s $dir/$file ~/.$file"
+    echo "Creating symlink to $dir/$file in ~/"
     ln -s $dir/$file ~/.$file
 done
